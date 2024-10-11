b70a8e4a4f7388c69a97f118d1b09317a5ef3235
Selig Razvan <razvanselig02@gmail.com>
2024-10-11 20:59:36 +0300

    Implemented the Rock Paper Scissors in a HTML file

---
+++ Exercise-3/main.js
@@ -0,0 +1,60 @@
+console.log("The JS file works");
+
+const htmlComputerElement = document.querySelector("#computer-choice")
+const htmlUserElement = document.querySelector("#user-choice")
+const htmlWinnerElement = document.querySelector("#round-result")
+
+
+function computerChoice() {
+    const options = ["rock", "paper", "scissors"]
+    let randomOption = Math.floor(Math.random() * options.length)
+
+    htmlComputerElement.textContent = `${options[randomOption]}`
+
+    return options[randomOption]
+
+    
+}
+
+
+
+function userChoice() {
+    
+    const options = ["rock", "paper", "scissors"]
+    
+    let userInput =  prompt("Pick rock, paper or scissors: ")
+    userInput = userInput.toLowerCase()
+
+    
+
+    while (!options.includes(userInput)) {
+        alert("Invalid Input")
+        userInput =  prompt("Pick rock, paper or scissors: ")
+    } 
+
+    htmlUserElement.textContent = `${userInput}`
+
+    let computerInput = computerChoice()
+
+    detectWiner(userInput, computerInput)
+
+}
+
+
+
+function detectWiner(userInput, computerInput) {
+
+    if (userInput === computerInput) {
+        htmlWinnerElement.textContent = `It's a tie`
+    } else if (
+        (userInput == "rock" && computerInput == "scissors") ||
+        (userInput == "paper" && computerInput == "rock") ||
+        (userInput == "scissors" && computerInput == "paper") 
+    ) {
+        htmlWinnerElement.textContent = `User wins`
+    } else {
+        htmlWinnerElement.textContent = `Computer wins`
+    }
+
+}
+
